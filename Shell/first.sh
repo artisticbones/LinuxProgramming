b6830f6873d3d9a568ebdc9cac7746147cad80e9
Cheng Feitian <artisticbones@163.com>
2020-03-13 22:24:42 +0800

    learn Shell

---
+++ Shell/first.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script displays the date and who`s logged on
+echo The time and date are:
+date
+echo "Let's see who's logged into the system:"
+who	# 可以在同一行 使用分号隔开两个命令，不在同一行，shell按照在文本文件中出现的顺序执行
+
